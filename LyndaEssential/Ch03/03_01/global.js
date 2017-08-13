0e494fe1d00a0b16055655ddaaadf422d99739e4
Rohit Raja Pahan <rohitp@getvymo.com>
2017-08-13 22:52:28 +0530

    first chapter first lession

---
+++ LyndaEssential/Ch03/03_01/global.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rohit on 13/08/17.
+ */
+var path = require("path");
+
+console.log("Foler name: "+__dirname);
+console.log("File path:"+__filename);
+console.log("File name:"+path.basename(__filename));
